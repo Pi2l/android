f85779c3921dd5d1214d63c506e85583b054d005
Liashenko Mykhailo <nightmisha8@gmail.com>
2023-02-13 10:26:58 +0200

    dice rolled 2

--- app/src/main/java/m/m/androidlab/MainActivity.kt
+++ app/src/main/java/m/m/androidlab/MainActivity.kt
@@ -3,6 +3,7 @@ package m.m.androidlab
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -16,8 +17,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val rolledText: TextView = findViewById(R.id.textView)
-        rolledText.text = Dice().roll().toString()
+        val rolledImage: ImageView = findViewById(R.id.imageView)
+        rolledImage.setImageResource(R.drawable.dice_1)
+
+        var rolledDice: Int = Dice().roll()
+        var imageToDraw: Int = when (rolledDice) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> { R.drawable.dice_6 }
+        }
+        rolledImage.contentDescription = rolledDice.toString()
+        rolledImage.setImageResource(imageToDraw)
+
     }
 }
 
