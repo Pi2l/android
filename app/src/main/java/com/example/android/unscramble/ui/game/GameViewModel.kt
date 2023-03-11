c5c7f77089442779b47651dd269a493aa2560d47
Liashenko Mykhailo <nightmisha8@gmail.com>
2023-03-11 14:20:34 +0200

    task 7: populate ViewModel

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -4,19 +4,49 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
-    init {
-        Log.d("GameFragment", "GameViewModel created")
-    }
-
     private var score = 0
     private var currentWordCount = 0
-    private var _currentScrambledWord = "test"
+    private var usedWords : MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
+    private lateinit var _currentScrambledWord: String
 
     val currentScrambledWord: String
         get() = _currentScrambledWord
 
+    init {
+        Log.d("GameFragment", "GameViewModel created")
+        getNextWord()
+    }
+
     override fun onCleared() {
         super.onCleared()
         Log.d("GameFragment", "GameViewModel created")
+    }
+
+    fun getNextWord() {
+        currentWord = allWordsList.random()
+        while (usedWords.contains(currentWord)) {
+            currentWord = allWordsList.random()
+        }
+
+        var tmpWord = currentWord.toCharArray()
+        tmpWord.shuffle()
+        while ( String(tmpWord).equals(currentWord, false) ) {
+            tmpWord.shuffle()
+        }
+        _currentScrambledWord = String(tmpWord)
+        ++currentWordCount
+        usedWords.add(_currentScrambledWord)
+    }
+
+    /*
+    * Returns true if the current word count is less than MAX_NO_OF_WORDS.
+    * Updates the next word.
+    */
+    fun nextWord(): Boolean {
+        return if (currentWordCount < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        } else false
     }
 }
