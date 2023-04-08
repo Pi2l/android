8a0b0c31bc252ed5fa336adbda9052535dfde7db
Liashenko Mykhailo <nightmisha8@gmail.com>
2023-04-08 21:57:57 +0300

    task 4[step 3-4]: Update the layout and fragments

--- app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
+++ app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
@@ -17,3 +17,17 @@
 
 package com.example.android.marsrealestate
 
+import android.widget.ImageView
+import androidx.core.net.toUri
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+
+@BindingAdapter("imageUrl")
+fun bindImage(imageView: ImageView, url: String?) {
+    url?.let {
+        val imgUri = url.toUri().buildUpon().scheme("https").build()
+        Glide.with(imageView.context)
+            .load(imgUri)
+            .into(imageView)
+    }
+}
