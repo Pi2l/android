00729dcf6fc0b01f1732c96215b9cf9fd625b06a
Liashenko Mykhailo <nightmisha8@gmail.com>
2023-04-08 22:09:24 +0300

    task 3: display an internet image

--- app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
+++ app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
@@ -21,6 +21,7 @@ import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 
 @BindingAdapter("imageUrl")
 fun bindImage(imageView: ImageView, url: String?) {
@@ -28,6 +29,10 @@ fun bindImage(imageView: ImageView, url: String?) {
         val imgUri = url.toUri().buildUpon().scheme("https").build()
         Glide.with(imageView.context)
             .load(imgUri)
+            .apply( RequestOptions()
+                .placeholder(R.drawable.loading_animation)
+                .error(R.drawable.ic_connection_error)
+            )
             .into(imageView)
     }
 }
