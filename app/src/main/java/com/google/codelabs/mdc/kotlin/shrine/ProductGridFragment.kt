3de1d08b196809d03297648b9018f4fe07f33b04
Liashenko Mykhailo <nightmisha8@gmail.com>
2023-04-22 15:35:25 +0300

    task 5: Create a grid of cards

--- app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
+++ app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
@@ -8,7 +8,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.codelabs.mdc.kotlin.shrine.network.ProductEntry
 import kotlinx.android.synthetic.main.shr_product_grid_fragment.view.app_bar
+import kotlinx.android.synthetic.main.shr_product_grid_fragment.view.recycler_view
 
 class ProductGridFragment : Fragment() {
 
@@ -18,13 +22,25 @@ class ProductGridFragment : Fragment() {
     }
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment with the ProductGrid theme
         val view = inflater.inflate(R.layout.shr_product_grid_fragment, container, false)
 
+        // Set up the toolbar.
         (activity as AppCompatActivity).setSupportActionBar(view.app_bar)
 
-        return view
+        // Set up the RecyclerView
+        view.recycler_view.setHasFixedSize(true)
+        view.recycler_view.layoutManager = GridLayoutManager(context, 2, RecyclerView.VERTICAL, false)
+        val adapter = ProductCardRecyclerViewAdapter(
+            ProductEntry.initProductEntryList(resources))
+        view.recycler_view.adapter = adapter
+        val largePadding = resources.getDimensionPixelSize(R.dimen.shr_product_grid_spacing)
+        val smallPadding = resources.getDimensionPixelSize(R.dimen.shr_product_grid_spacing_small)
+        view.recycler_view.addItemDecoration(ProductGridItemDecoration(largePadding, smallPadding))
+
+        return view;
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu, menuInflater: MenuInflater) {
